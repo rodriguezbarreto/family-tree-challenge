4816e725ec466003df341681e54caf41297b1904
rodriguesbarreto <munizbarreto5@gmail.com>
2023-08-08 17:27:54 -0300

    feat: add errors validators

---
+++ internal/domain/internal-errors/validator.go
@@ -0,0 +1,33 @@
+package internalerrors
+
+import (
+	"errors"
+	"strings"
+
+	"github.com/go-playground/validator/v10"
+)
+
+func ValidatorStruct(obj interface{}) error {
+	validate := validator.New()
+	err := validate.Struct(obj)
+
+	if err == nil {
+		return nil
+	}
+
+	validationErrors := err.(validator.ValidationErrors)
+	validationError := validationErrors[0]
+
+	field := strings.ToLower(validationError.StructField())
+
+	switch validationError.Tag() {
+	case "required":
+		return errors.New(field + " is required")
+
+	case "min":
+		return errors.New(field + " is required with min " + validationError.Param())
+	}
+
+	return nil
+
+}
