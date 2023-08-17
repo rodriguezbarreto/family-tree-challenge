1875485cf69a0f6358a964069478528f2c2fac9b
rodriguesbarreto <munizbarreto5@gmail.com>
2023-08-17 04:04:32 -0300

    feat: add env variables in connection database

--- internal/infra/database/postgres-connection.go
+++ internal/infra/database/postgres-connection.go
@@ -3,19 +3,20 @@ package database
 import (
 	"family-tree-challenge/internal/domain"
 	"fmt"
+	"os"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 func PostgresConnection() *gorm.DB {
-	host := "localhost"
-	port := 5432
-	user := "user"
-	password := "password"
-	dbname := "database"
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbname := os.Getenv("DB_NAME")
 
-	dsn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
 	if err != nil {
@@ -26,5 +27,3 @@ func PostgresConnection() *gorm.DB {
 
 	return db
 }
-
-// TODO CRIAR VARIAVEIS DE AMBIENTE
