cffb7cb4eed0362c30a70847d0864c52020f47db
rodriguesbarreto <munizbarreto5@gmail.com>
2023-08-18 14:53:10 -0300

    refactor: adjustment seed database module

--- Dockerfile
+++ Dockerfile
@@ -5,19 +5,21 @@ WORKDIR /app
 
 COPY . .
 
-COPY .env .env
-
 RUN go mod download
 
 RUN CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o main ./cmd/main.go
+
+RUN CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o seed ./cmd/seed/seed.go
 
 FROM alpine:latest
 
 WORKDIR /app
 
-COPY --from=builder /app/.env /app/.env
-
 COPY --from=builder /app/main /app/main
+
+COPY --from=builder /app/seed /app/seed
+
+COPY --from=builder /app/.env /app/.env
 
 EXPOSE 8080
 
--- cmd/seed/seed.go
+++ cmd/seed/seed.go
@@ -1,20 +1,79 @@
 package main
 
 import (
-	"family-tree-challenge/cmd/configs"
+	"family-tree-challenge/internal/domain"
 	"family-tree-challenge/internal/infra/database"
+	"family-tree-challenge/internal/infra/database/repositories"
 	"log"
+
+	"gorm.io/gorm"
 )
 
 func main() {
 	println("Seeding database...")
 	db := database.PostgresConnection()
-	err := configs.SeedDatabase(db)
+	err := SeedDatabase(db)
 	if err != nil {
 		log.Fatalf("Error seeding database: %s", err)
 		return
 	}
 	println("Database successfully seeded ✔")
 
+}
 
+func SeedDatabase(db *gorm.DB) error {
+	personRepo := repositories.NewPersonRepository(db)
+	relationshipRepo := repositories.NewRelationshipRepository(db)
+
+	peopleNames := []string{"Sonny", "Ann", "Dunny", "Bruce", "Advick", "Martin", "Phoebe", "Anastasia", "Clark", "Jaqueline", "Oprah", "Eric", "Ellen", "Ariel", "Melody"}
+	peopleMap := map[string]*domain.Person{}
+
+	for _, name := range peopleNames {
+		person, err := domain.NewPerson(name)
+		if err != nil {
+			return err
+		}
+		err = personRepo.Create(person)
+		if err != nil {
+			return err
+		}
+		peopleMap[name] = person
+	}
+
+	relations := []struct {
+		Parent string
+		Child  string
+	}{
+		{"Sonny", "Ann"},
+		{"Martin", "Phoebe"},
+		{"Anastasia", "Phoebe"},
+		{"Anastasia", "Clark"},
+		{"Oprah", "Eric"},
+		{"Ellen", "Eric"},
+		{"Ann", "Dunny"},
+		{"Ann", "Bruce"},
+		{"Advick", "Dunny"},
+		{"Advick", "Bruce"},
+		{"Phoebe", "Bruce"},
+		{"Phoebe", "Dunny"},
+		{"Clark", "Jaqueline"},
+		{"Eric", "Jaqueline"},
+		{"Eric", "Melody"},
+		{"Ariel", "Melody"},
+	}
+
+	for _, relation := range relations {
+		parent := peopleMap[relation.Parent]
+		child := peopleMap[relation.Child]
+		relationship, err := domain.NewRelationship(*child, *parent)
+		if err != nil {
+			return err
+		}
+		err = relationshipRepo.Create(relationship)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
